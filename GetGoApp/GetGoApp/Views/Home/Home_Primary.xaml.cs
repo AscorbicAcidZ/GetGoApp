4003c282d5c44b876c4e5029b51b0851151034b8
Reiji <reijideveloper@gmail.com>
2023-07-16 11:21:28 +0800

    added home

---
+++ GetGoApp/GetGoApp/Views/Home/Home_Primary.xaml.cs
@@ -0,0 +1,46 @@
+﻿using GetGoApp.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GetGoApp.Views.Home
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Home_Primary : ContentPage
+    {
+        private string link, name;
+      
+        public Home_Primary()
+        {
+          
+            InitializeComponent();
+
+            string details = AppData.Instance.Details;
+            if (!string.IsNullOrEmpty(details))
+            {
+                string[] detailsArray = details.Split('|');
+                if (detailsArray.Length >= 2)
+                {
+                    link = detailsArray[0];
+                    name = detailsArray[1];
+                }
+            }
+            webView.Source = new UrlWebViewSource { Url = $"{link}/Views/UserApp/Profile/Profile_Primary.aspx" };
+        }
+        private void HomeButton_Clicked(object sender, EventArgs e)
+        {
+        
+            Navigation.PushAsync(new Views.Home.Home_Primary());
+        }
+
+        private void MenuButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Views.Menu());
+        }
+    }
+}
