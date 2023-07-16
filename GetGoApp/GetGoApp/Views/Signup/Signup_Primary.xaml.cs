56ff364e3b5724bed12cbc1c42e268167f164d98
Reiji <reijideveloper@gmail.com>
2023-07-16 11:20:11 +0800

    added signup

---
+++ GetGoApp/GetGoApp/Views/Signup/Signup_Primary.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GetGoApp.Views.Signup
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Signup_Primary : ContentPage
+    {
+        public Signup_Primary()
+        {
+            InitializeComponent();
+        }
+    }
+}
