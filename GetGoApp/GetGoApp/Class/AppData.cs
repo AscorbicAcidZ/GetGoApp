0350c6fe94f00568cdb0e99de9a41d4dce1e99f5
Reiji <reijideveloper@gmail.com>
2023-07-16 11:21:09 +0800

    added global variable

---
+++ GetGoApp/GetGoApp/Class/AppData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetGoApp.Class
+{
+    public class AppData
+    {
+        private static AppData _instance;
+        public string Details { get; set; }
+
+        private AppData() { }
+
+        public static AppData Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new AppData();
+                return _instance;
+            }
+        }
+    }
+}
