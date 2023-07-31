afeb53a8a584fa992b9eee3cd8b0847dba34e8ab
Reiji <reijideveloper@gmail.com>
2023-07-31 22:25:33 +0800

    added signup

--- GetGoApp/GetGoApp/Views/Signup/Signup_Secondary.xaml.cs
+++ GetGoApp/GetGoApp/Views/Signup/Signup_Secondary.xaml.cs
@@ -11,26 +11,39 @@ using Xamarin.Forms.Xaml;
 namespace GetGoApp.Views.Signup
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Signup_Primary : ContentPage
+    public partial class Signup_Secondary : ContentPage
     {
-        private string link;
-        public Signup_Primary()
-        {   
+        private string link,signupDetails,userName,email,phoneNumber;
+        public Signup_Secondary()
+        {
             InitializeComponent();
             link = AppData.Instance.Link;
-
+            signupDetails = AppData.Instance.SignupDetails;
+            if (!string.IsNullOrEmpty(signupDetails))
+            {
+                string[] detailsArray = signupDetails.Split('|');
+                if (detailsArray.Length >= 2)
+               {
+                    userName = detailsArray[1];
+                    phoneNumber = detailsArray[2];
+                    email = detailsArray[3];
+                }
+            }
             if (!string.IsNullOrEmpty(link))
             {
-                webView.Source = new UrlWebViewSource { Url = $"{link}/Views/UserApp/Signup/Signup_Primary.aspx" };
+                webView.Source = new UrlWebViewSource 
+                { Url = $"{link}/Views/UserApp/Signup/Signup_Secondary.aspx?UserName={userName}&PhoneNumber={phoneNumber}&Email={email}" };
             }
+
+            
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private void Save_Clicked(object sender, EventArgs e)
         {
             webView.EvaluateJavaScriptAsync("SaveClick();");
             webView.Navigated += WebView_Navigated;
         }
-        private async void WebView_Navigated(object sender, WebNavigatedEventArgs e)
+        private  void WebView_Navigated(object sender, WebNavigatedEventArgs e)
         {
             // Check if the navigation was successful and the URL is not null or empty
             if (e.Result == WebNavigationResult.Success && !string.IsNullOrEmpty(e.Url))
@@ -45,7 +58,7 @@ namespace GetGoApp.Views.Signup
                 // Set the SignupDetails value in the global data storage class
                 AppData.Instance.SignupDetails = signupDetails;
 
-                await Navigation.PushAsync(new Signup_Secondary());
+                //await Navigation.PushAsync(new Signup_Secondary());
                 // Do something with the query string values here...
             }
         }
@@ -59,5 +72,6 @@ namespace GetGoApp.Views.Signup
             }
             return signupDetails.ToString();
         }
+
     }
 }
