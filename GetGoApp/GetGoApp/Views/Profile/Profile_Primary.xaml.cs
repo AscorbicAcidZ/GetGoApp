f7df6156a013fd84cb642db38cfd92bca471584c
Reiji <reijideveloper@gmail.com>
2023-08-06 21:48:52 +0800

    profile fix code

--- GetGoApp/GetGoApp/Views/Profile/Profile_Primary.xaml.cs
+++ GetGoApp/GetGoApp/Views/Profile/Profile_Primary.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GetGoApp.Class;
+using GetGoApp.Views.Signup;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,81 @@ namespace GetGoApp.Views.Profile
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Profile_Primary : ContentPage
     {
+        private string link, signupDetails, userName, email, phoneNumber, Details,userId;
         public Profile_Primary()
         {
             InitializeComponent();
+
+            link = AppData.Instance.Link;
+            signupDetails = AppData.Instance.SignupDetails;
+            if (!string.IsNullOrEmpty(signupDetails))
+            {
+                string[] detailsArray = signupDetails.Split('|');
+                if (detailsArray.Length >= 2) // Make sure there are enough elements
+                {
+                    userName = detailsArray[1].Replace(":", "=");
+                    phoneNumber = detailsArray[2].Replace(":", "=");
+                    email = detailsArray[3].Replace(":", "=");
+                }
+                if (!string.IsNullOrEmpty(link))
+                {
+                    webView.Source = new UrlWebViewSource
+                    { Url = $"{link}/Views/UserApp/Profile/Profile_Primary.aspx?{userName}&{phoneNumber}" };
+                }
+            }
+            else
+            {
+
+                Details = AppData.Instance.Details;
+                if (!string.IsNullOrEmpty(Details))
+                {
+                    string[] detailsArray = Details.Split('|');
+                    if (detailsArray.Length >= 2)
+                    {
+                        userId = detailsArray[1];
+                        
+                    }
+                    webView.Source = new UrlWebViewSource
+                    { Url = $"{link}/Views/UserApp/Profile/Profile_Primary.aspx.aspx?{userId}" };
+                }
+            }
+
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            webView.EvaluateJavaScriptAsync("SaveClick();");
+            webView.Navigated += WebView_Navigated;
+        }
+        private async void WebView_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            // Check if the navigation was successful and the URL is not null or empty
+            if (e.Result == WebNavigationResult.Success && !string.IsNullOrEmpty(e.Url))
+            {
+                // Get the query string parameters from the URL
+                var uri = new Uri(e.Url);
+                var queryParameters = System.Web.HttpUtility.ParseQueryString(uri.Query);
+                var parametersDict = queryParameters.AllKeys.ToDictionary(key => key, key => queryParameters[key]);
+                // Build the SignupDetails string
+                string signupDetails = BuildSignupDetails(link, parametersDict);
+
+                // Set the SignupDetails value in the global data storage class
+                AppData.Instance.SignupDetails = signupDetails;
+
+                await Navigation.PushAsync(new Profile_Secondary());
+                // Do something with the query string values here...
+            }
+        }
+        private string BuildSignupDetails(string link, Dictionary<string, string> queryParameters)
+        {
+            StringBuilder signupDetails = new StringBuilder();
+            signupDetails.Append($"{link}|");
+            foreach (var parameter in queryParameters)
+            {
+                signupDetails.Append($"{parameter.Key}:{parameter.Value}|");
+            }
+            return signupDetails.ToString();
         }
     }
 }
